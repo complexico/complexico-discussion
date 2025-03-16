3afa09969d82113f63bf5bc95f2c4adc4e81c5d5
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-03-16 17:36:45 +0800

    Add the .sh file.

---
+++ complexico-discussion-bash.sh
@@ -0,0 +1,19 @@
+mkdir complexico-discussion
+
+cd complexico-discussion
+
+git init .
+
+touch README.md
+
+nano README.md
+
+git add README.md
+
+git commit -m "First commit."
+
+git branch -M main
+
+git remote add origin git@github.com:gederajeg/complexico-discussion.git
+
+git push -u origin main
